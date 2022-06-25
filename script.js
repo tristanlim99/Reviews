34a006d28b9e8f696be90ff1d5d70bb9aba0c857
Tristan Yves Lim <94602671+tristanlim99@users.noreply.github.com>
2022-06-25 17:22:04 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -44,39 +44,43 @@ const randomBtn = document.querySelector(".random-btn");
 let currentItem = 0;
 
 //load initial item
+//onLoad event listener so that the function will load on window
 window.addEventListener("DOMContentLoaded", function () {
-  showPerson(currentItem);
+  showPerson(currentItem); // Callback function showPerson with currentItem as parameter
 });
 
 //show person based on item
-
+//function focuses on the changes that will be made on the reviews var
 const showPerson = (person) => {
-  const item = reviews[person];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
+  const item = reviews[person]; //block variable to target the reviews var
+  img.src = item.img; //block variable to target the reviews img
+  author.textContent = item.name; //block variable to target the reviews name
+  job.textContent = item.job; //block variable to target the reviews job
+  info.textContent = item.text; //block variable to target the reviews paragraph
 };
 
-//show next person
+//click event listener to show/ transition to next person
 nextBtn.addEventListener("click", function () {
-  currentItem++;
+  currentItem++; //increment the current item per click
+  //if currentItem is greater than the reviews' length minus 1
+  //currentItem will reset to 0
   if (currentItem > reviews.length - 1) {
     currentItem = 0;
   }
-  showPerson(currentItem);
+  showPerson(currentItem); //Callback function to showPerson
 });
-//show previous person
+//click event to show/transition to previous person
 prevBtn.addEventListener("click", function () {
-  currentItem--;
+  currentItem--; //decrement currentItem value per click
+  //if currentItem is less than 0, currentItem should be equal to reviews' length minus 1
   if (currentItem < 0) {
     currentItem = reviews.length - 1;
   }
-  showPerson(currentItem);
+  showPerson(currentItem); //Callback function to showPerson
 });
 
-//show random person
+//click event listener to show random person
 randomBtn.addEventListener("click", function () {
-  currentItem = Math.floor(Math.random() * reviews.length);
-  showPerson(currentItem);
+  currentItem = Math.floor(Math.random() * reviews.length); //rounding up the random numbers by multiplying reviews' length
+  showPerson(currentItem); //Callback function to showPerson
 });
